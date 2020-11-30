e73f3c314a65daee52eeda11f1f8e12dccb6b696
catwhitmer <catwhitmer@gmail.com>
2020-11-30 15:36:44 -0500

    used prettier extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ToDoList from './components/ToDoList';
+import NewTodo from "./components/NewTodo";
+import React from "react";
+import ToDoList from "./components/ToDoList";
 
 function App() {
-  const todos = [{id: 't1', text: 'Finish the course'}];
+  const todos = [{ id: "t1", text: "Finish the course" }];
   return (
     <div className="App">
-      <ToDoList items={todos}/>
+      <NewTodo />
+      <ToDoList items={todos} />
     </div>
   );
 }
